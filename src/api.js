513e36321adf1a1c950ea1c8b895b5fed1b5fdba
Esha <maheshwariesha11@gmail.com>
2025-01-25 01:49:29 +0530

    added deployed base url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'https://task-management-4tz8.onrender.com/api/v1',
 });
 
 export default axiosInstance;
